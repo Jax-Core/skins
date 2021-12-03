4e921173e53d1ec1dd150678e31329695ccdc3b9
Shaktijeet Sahoo <sahooshaktijeet@gmail.com>
2021-12-03 20:39:14 +0530

    Initited Skins page

---
+++ js/main.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+	const cards = document.querySelector('#preview-cards')
+	const skinsJson = JSON.parse(skindata)
+
+	let cardTemplate = document.getElementById('cardTemplate')
+
+	skinsJson.forEach((skin) => {
+		let cardClone = cardTemplate.content.cloneNode(true)
+
+		cardClone.querySelector('.image').querySelector('.preview-image').src =
+			skin.background
+		cardClone.querySelector('.title').textContent = skin.title
+		cardClone.querySelector('.subtitle').textContent = skin.subs
+		cardClone.querySelector('.skinDesc').textContent = skin.description
+
+		cards.appendChild(cardClone)
+	})
+
+	const cardBoxes = document.querySelectorAll('#preview-cards .box')
+
+	cardBoxes.forEach((card) => {
+		card.addEventListener('click', (event) => {
+			console.log(card)
+			ShowMainPreview({
+				title: card.querySelector('.title').textContent,
+				subs: card.querySelector('.subtitle').textContent,
+				description: card.querySelector('.skinDesc').textContent,
+				image: card
+					.querySelector('.image')
+					.querySelector('.preview-image').src,
+			})
+			window.scrollTo({ top: 0, behavior: 'smooth' })
+		})
+	})
+})
+
+var mainPreviewTitle = document.querySelector('#main-preview-title')
+var mainPreviewSubtitle = document.querySelector('#main-preview-subtitle')
+var mainPreviewDescription = document.querySelector('#main-preview-description')
+var mainPreviewStats = document.querySelector('#main-preview-stats')
+var mainPreviewImage = document.querySelector('#main-preview-image')
+
+function ShowMainPreview(data) {
+	mainPreviewTitle.querySelector('strong').textContent = data.title
+	mainPreviewSubtitle.textContent = data.subs
+	mainPreviewDescription.textContent = data.description
+	mainPreviewImage.src = data.image
+	mainPreviewStats.style.display = 'flex'
+}
