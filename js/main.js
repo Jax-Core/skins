ab04fcb2c2b00e7fc38683db3b003c1ccaef8f8a
Shaktijeet Sahoo <sahooshaktijeet@gmail.com>
2021-12-04 15:45:45 +0530

    Changed almost everything

--- js/main.js
+++ js/main.js
@@ -13,12 +13,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
 		cardClone.querySelector('.subtitle').textContent = skin.subs
 		cardClone.querySelector('.skinDesc').textContent = skin.description
 
+		let box = cardClone.querySelector('.box')
+
+		box.setAttribute('data-deviantart', skin.deviantart)
+		box.setAttribute('data-download', skin.download_api)
+		box.setAttribute('data-install', skin.install_name)
+
 		cards.appendChild(cardClone)
 	})
 
 	const cardBoxes = document.querySelectorAll('#preview-cards .box')
 
-    cardBoxes.forEach((card) => {
+	cardBoxes.forEach((card) => {
 		$clamp(card.querySelector('.skinDesc'), { clamp: 3 })
 		card.addEventListener('click', (event) => {
 			ShowMainPreview({
@@ -28,29 +34,95 @@ document.addEventListener('DOMContentLoaded', (event) => {
 				image: card
 					.querySelector('.image')
 					.querySelector('.preview-image').src,
+				deviantart: card.getAttribute('data-deviantart'),
+				download: card.getAttribute('data-download'),
+				install: card.getAttribute('data-install'),
 			})
-			window.scrollTo({ top: 0, behavior: 'smooth' })
 		})
 	})
 })
 
+var modal = document.getElementById('main-modal')
 var mainPreviewTitle = document.querySelector('#main-preview-title')
 var mainPreviewSubtitle = document.querySelector('#main-preview-subtitle')
 var mainPreviewDescription = document.querySelector('#main-preview-description')
-var mainPreviewStats = document.querySelector('#main-preview-stats')
 var mainPreviewImage = document.querySelector('#main-preview-image')
+var deviantart = document.getElementById('deviantart-button')
+var skindownload = document.getElementById('download-button')
+var skininstall = document.getElementById('install-button')
+
+if (navigator.userAgent.indexOf('Win') == -1) {
+	skininstall.style.display = 'none'
+}
+
+var modalBack = document.getElementById('modal-background')
+
+modalBack.addEventListener('click', (event) => {
+	modal.classList.add('is-inactive')
+	setTimeout(() => {
+		modal.classList.remove('is-active')
+	}, 450)
+})
 
 function ShowMainPreview(data) {
-	mainPreviewTitle.querySelector('strong').textContent = data.title
+	mainPreviewTitle.textContent = data.title
 	mainPreviewSubtitle.textContent = data.subs
 	mainPreviewDescription.textContent = data.description
 	mainPreviewImage.src = data.image
-	mainPreviewStats.style.display = 'flex'
+
+	deviantart.addEventListener('click', (event) => {
+		window.open(data.deviantart)
+	})
+
+	skindownload.addEventListener('click', (event) => {
+		if (navigator.userAgent.indexOf('Win') == -1) {
+			Swal.fire({
+				title: '<p style="color: #facea8">Incompatible Device</p>',
+				html: 'This software is intended for Windows devices only. Download anyway?',
+				icon: 'warning',
+				imageWidth: 128,
+				imageHeight: 128,
+				background: '#181a1b',
+				showDenyButton: true,
+				confirmButtonText: 'Download',
+				denyButtonText: 'Cancel',
+			}).then((result) => {
+				if (result.isConfirmed) {
+					DownloadLatestSkin(data.download)
+				}
+			})
+		} else {
+			DownloadLatestSkin(data.download)
+		}
+	})
+
+	skininstall.addEventListener('click', (event) => {
+		location.href = `rm-coreinstaller:[!ActivateConfig "#JaxCore\\CoreInstaller"][!CommandMeasure DelayedBanger "GetSkin('${data.install}')" "#JaxCore\\CoreInstaller"`
+	})
+
+	modal.classList.remove('is-inactive')
+	modal.classList.add('is-active')
 }
 
 let navScrollElements = document.querySelectorAll(
 	'.navbar.has-background-transparent'
 )
+
+function DownloadLatestSkin(api_url) {
+	skindownload.classList.add('is-loading')
+	fetch(api_url)
+		.then((response) => response.json())
+		.then((api_data) => {
+			api_data.assets.forEach((asset) => {
+				if (asset.browser_download_url.indexOf('.rmskin') != -1) {
+					location.href = asset.browser_download_url
+				}
+			})
+		})
+		.then(() => {
+			skindownload.classList.remove('is-loading')
+		})
+}
 
 window.addEventListener('scroll', (event) => {
 	var scrollTop =
