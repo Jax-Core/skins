30db1216427465a19d005c091efcb9a4f7ad99bc
Shaktijeet Sahoo <sahooshaktijeet@gmail.com>
2021-12-04 16:23:00 +0530

    completed navbar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,44 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+	// hamburger
+	// Get all "navbar-burger" elements
+	const $navbarBurgers = Array.prototype.slice.call(
+		document.querySelectorAll('.navbar-burger'),
+		0
+	)
+
+	// Check if there are any navbar burgers
+	if ($navbarBurgers.length > 0) {
+		// Add a click event on each of them
+		$navbarBurgers.forEach((el) => {
+			let children = el.children
+			// Get the target from the "data-target" attribute
+			const target = el.dataset.target
+			const $target = document.getElementById(target)
+			el.addEventListener('click', function (event) {
+				console.log(event.currentTarget)
+				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+				el.classList.toggle('is-active')
+				$target.classList.toggle('is-active')
+			})
+		})
+	}
+
+	// link buttons
+	const navlogo = document.querySelector('.navbar-item img')
+	navlogo.addEventListener('click', () => {
+		window.location.href = 'https://jax-core.github.io'
+	})
+
+	// donation button
+	const donation = document.getElementById('donation')
+	donation.addEventListener('click', () => {
+		window.open(
+			'https://ko-fi.com/jaxoriginals',
+			'Jax - KoFi',
+			'height=500, width=400'
+		)
+	})
+
 	const cards = document.querySelector('#preview-cards')
 	const skinsJson = JSON.parse(skindata)
 
