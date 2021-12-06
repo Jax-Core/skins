1d586a2cfab7345aaf1728ebab0da569bf395782
Jax <80020581+EnhancedJax@users.noreply.github.com>
2021-12-06 19:33:45 +0800

    Update skins.js

--- data/skins.js
+++ data/skins.js
@@ -2,7 +2,7 @@ const modules_data = `[
     {
         "title": "IdleStyle",
 		"subs": "Screensaver reimagined.",
-		"description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, quis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, quis.",
+		"description": "A gallery of custom and pre-made screensavers to stylized your idle desktop, featuring Jelle Dekker's weather skin. If you want to customize it further, you can choose skins on your device to use in IdleStyle with minimal setup. Got an awesome loop that you want to use? Gotchu covered. Tired of your lifeless desktop? Choose one of the animated styles! Get IdleStyle today!",
 		"background": "img/skins/IdleStyle.png",
         "deviantart": "https://www.deviantart.com/jaxoriginals/art/IdleStyle-Stylized-Screensaver-899004964",
         "download_api": "https://api.github.com/repos/EnhancedJax/IdleStyle/releases/latest",
@@ -11,52 +11,43 @@ const modules_data = `[
 	{
 		"title": "KeyLaunch",
 		"subs": "Launch apps with your keyboard.",
-		"description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, quis.",
+		"description": "Ever wanted to launch apps quickly without having to move your mouse to the taskbar or search for it on your desktop? Well now you can launch apps easily with just a press of a hotkey! The applications list is extremely easy to setup, just click add and use the quick start to autofill all the application details! You can even add custom actions and change the name / icon of the application! There are a total of 4 types of animation, you can choose which one you want to suit your needs.",
 		"background": "img/skins/KeyLaunch.png",
         "deviantart": "https://www.deviantart.com/jaxoriginals/art/Keylaunch-1-45-890749449",
         "download_api": "https://api.github.com/repos/EnhancedJax/KeyLaunch/releases/latest",
         "install_name": "KeyLaunch"
 	},
 	{
-		"title": "KeyStrokes",
-		"subs": "Stream your keys strokes with style.",
-		"description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt architecto eaque aliquid perferendis saepe ipsa recusandae tempore perspiciatis non est quis vel, vitae reiciendis magnam voluptatum obcaecati, consequuntur sequi, distinctio cum nisi voluptas dolores fuga unde minima. Enim accusamus fugit, possimus tenetur minus praesentium veritatis, aliquam officia quod ex sequi sit eum libero porro, esse incidunt quaerat maiores molestias.",
-		"background": "img/skins/KeyStrokes.png",
-        "deviantart": "https://www.deviantart.com/jaxoriginals/art/Keystrokes-v1-3-889349339",
-        "download_api": "https://api.github.com/repos/EnhancedJax/KeyStrokes/releases/latest",
-        "install_name": "KeyStrokes"
-	},
-	{
 		"title": "QuickNote",
-		"subs": "Quickly access your notes.",
-		"description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, quis.",
-		"background": "img/skins/QuickNote.png",
+		"subs": "Quick access notepad.",
+		"description": "A quick note module to improve your productivity. QuickNote allows easy row manipulation and comes with some smooth transitional animations included. It also has a built-in checklist feature which you can use to make a to-do list. You have access to 5 different slots for saving your notes. You can also choose between a custom hotkey and dragging from the top-left corner of your screen to summon the module.",
+		"background": "img/skins/KeyStrokes.png",
         "deviantart": "https://www.deviantart.com/jaxoriginals/art/QuickNote-Quick-access-notepad-894951390",
         "download_api": "https://api.github.com/repos/EnhancedJax/QuickNote/releases/latest",
         "install_name": "QuickNote"
 	},
-    {
-        "title": "ValliStart",
+	{
+		"title": "ValliStart",
 		"subs": "A modern start menu replacement.",
-		"description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, quis.",
-		"background": "img/skins/ValliStart.png",
+		"description": "This module replaces the default windows start menu with an animated, fancy control center. This module provides 8 different color schemes for you to use, and have 15+ customization options for you to play with! By default the module overrides the Win hotkey, you can choose to turn this off. This doesn't override the start button though.",
+		"background": "img/skins/QuickNote.png",
         "deviantart": "https://www.deviantart.com/jaxoriginals/art/ValliStart-Start-menu-replacement-893506095",
         "download_api": "https://api.github.com/repos/EnhancedJax/ValliStart/releases/latest",
         "install_name": "ValliStart"
+	},
+    {
+        "title": "MIUI-Shade",
+		"subs": "MIUI dropdown menu, on the desktop.",
+		"description": "A rainmeter module which mimics the MIUI dropdown menu. This module is now depreciated: there will no longer be any updates to the module.",
+		"background": "img/skins/MIUI-Shade.png",
+        "deviantart": "https://www.deviantart.com/jaxoriginals/art/MIUI-Shade-v2-0-885183361",
+        "download_api": "https://api.github.com/repos/EnhancedJax/MIUI-Shade/releases/latest",
+        "install_name": "MIUI-Shade"
     }
 ]`
 
 const widgets_data = `[
     {
-        "title": "CombiLaunch",
-		"subs": "Launch with combinations.",
-		"description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt architecto eaque aliquid perferendis saepe ipsa recusandae tempore perspiciatis non est quis vel, vitae reiciendis magnam voluptatum obcaecati, consequuntur sequi, distinctio cum nisi voluptas dolores fuga unde minima. Enim accusamus fugit, possimus tenetur minus praesentium veritatis, aliquam officia quod ex sequi sit eum libero porro, esse incidunt quaerat maiores molestias.",
-		"background": "img/skins/Combilaunch.png",
-        "deviantart": "https://www.deviantart.com/jaxoriginals/art/Combilaunch-v1-3-881177085",
-        "download_api": "https://api.github.com/repos/EnhancedJax/Combilaunch/releases/latest",
-        "install_name": "Combilaunch"
-    },
-	{
 		"title": "ModularClocks",
 		"subs": "A modern clock for your screen.",
 		"description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, quis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, quis.",
@@ -82,5 +73,14 @@ const widgets_data = `[
         "deviantart": "https://www.deviantart.com/jaxoriginals/art/Plainext-v1-8-881628513",
         "download_api": "https://api.github.com/repos/EnhancedJax/Plainext/releases/latest",
         "install_name": "Plainext"
-	}
+	},
+	{
+        "title": "CombiLaunch",
+		"subs": "Launch with combinations.",
+		"description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt architecto eaque aliquid perferendis saepe ipsa recusandae tempore perspiciatis non est quis vel, vitae reiciendis magnam voluptatum obcaecati, consequuntur sequi, distinctio cum nisi voluptas dolores fuga unde minima. Enim accusamus fugit, possimus tenetur minus praesentium veritatis, aliquam officia quod ex sequi sit eum libero porro, esse incidunt quaerat maiores molestias.",
+		"background": "img/skins/Combilaunch.png",
+        "deviantart": "https://www.deviantart.com/jaxoriginals/art/Combilaunch-v1-3-881177085",
+        "download_api": "https://api.github.com/repos/EnhancedJax/Combilaunch/releases/latest",
+        "install_name": "Combilaunch"
+    }
 ]`
